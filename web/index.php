7421129596428d4756c6a9ac81ea5edd8856d3c0
int <int@int-pc.(none)>
2012-08-09 16:58:41 +0400

    Correct namespace

--- web/index.php
+++ web/index.php
@@ -1,23 +1,19 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd"> 
-<html xmlns="http://www.w3.org/1999/xhtml">
+ <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <link rel="stylesheet" type="text/css" href="../css/style.css">
-<title>Заказ еды</title>
+<title>Заказ обедов</title>
 <meta http-equiv="Content-Type" content="text/html;" />
 </head>
 <body>
 <?php
-
-use src\Controllers\Controller;
-
 error_reporting(E_ALL ^ E_NOTICE);
-
-require("..\src\AutoLoader\AutoLoader.php");
+use Controllers\Controller;
+require_once("..\src\AutoLoader\AutoLoader.php");
 
 $control = new Controller();
 $control->processData();
 
 ?>
-</body>
-</html>
+
 
